761b5ce749793b1f6a3d4208c4a82baceacd790a
ZZJ <zhaozjseu@163.com>
2019-08-30 16:16:52 +0800

    添加配置文件，修改监听端口

--- config/config.default.js
+++ config/config.default.js
@@ -2,6 +2,10 @@
 
 'use strict';
 
+const yaml = require('yaml')
+const fs = require('fs')
+const path = require('path')
+
 /**
  * @param {Egg.EggAppInfo} appInfo app info
  */
@@ -10,7 +14,18 @@ module.exports = appInfo => {
    * built-in config
    * @type {Egg.EggAppConfig}
    **/
-  const config = exports = {};
+
+
+  let configYaml = yaml.parse(fs.readFileSync(path.join(appInfo.baseDir, 'SEU-TroubleShooting.yml'), 'utf8'))
+  
+  const config = exports = {
+    cluster: {
+      listen: {
+        port: configYaml.port ? +configYaml.port : 7942,
+        hostname: configYaml.hostname
+      }
+    }
+  };
 
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1567152122001_1182';
