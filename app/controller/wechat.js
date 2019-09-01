bfa019a3392f1bebae5a1b5c215f291d33098cc6
ZZJ <zhaozjseu@163.com>
2019-09-01 21:48:34 +0800

    加入vue前端

--- app/controller/wechat.js
+++ app/controller/wechat.js
@@ -3,6 +3,7 @@
 const Controller = require('egg').Controller;
 const sha1 = require('sha1');
 const moment = require('moment');
+const sprintf = require('sprintf-js').sprintf;
 
 class wechatController extends Controller {
     async checkSignature() {
@@ -25,18 +26,38 @@ class wechatController extends Controller {
 
     async post() {
         const { ctx } = this;
-        console.log(ctx.request.body);
-        ctx.response.type = 'application/xml';
-        ctx.response.body =
-        `
-        <xml>
-            <ToUserName><![CDATA[${ctx.request.body.FromUserName}]]></ToUserName>
-            <FromUserName><![CDATA[${ctx.request.body.ToUserName}]]></FromUserName>
-            <CreateTime>${moment().unix()}</CreateTime>
-            <MsgType><![CDATA[text]]></MsgType>
-            <Content><![CDATA[傻逼]]></Content>
-        </xml>
-        `
+
+        // let access_token = await ctx.service.getAccessToken.accessToken();
+        // let url = sprintf('https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token=%s', access_token);
+        // let result = await ctx.curl(url, {
+        //     method: 'POST',
+        //     contentType: 'json',
+        //     data: {
+        //         "touser": ctx.request.query.openid,
+        //         "msgtype": "text",
+        //         "text":
+        //         {
+        //             "content": "<a href='https://myseu.cn' data-miniprogram-appid='wxaef6d2413690047f' data-miniprogram-path='pages/idsAuth?APPID=<APPID>&IDS_SESSION=<IDS_SESSION>&FORCE=<FORCE>'>统一身份认证登录</a>"
+        //         }
+
+        //     },
+        //     dataType: 'json',
+        // })
+        // console.log(result);
+
+        // 被动回复消息
+        // console.log(ctx.request.body);
+        // ctx.response.type = 'application/xml';
+        // ctx.response.body =
+        // `
+        // <xml>
+        //     <ToUserName><![CDATA[${ctx.request.body.FromUserName}]]></ToUserName>
+        //     <FromUserName><![CDATA[${ctx.request.body.ToUserName}]]></FromUserName>
+        //     <CreateTime>${moment().unix()}</CreateTime>
+        //     <MsgType><![CDATA[text]]></MsgType>
+        //     <Content><![CDATA[<a href="http://www.qq.com" data-miniprogram-appid="wxaef6d2413690047f" data-miniprogram-path="pages/index/index">点击跳小程序</a>]]></Content>
+        // </xml>
+        // `
     }
 }
 
--- app/router.js
+++ app/router.js
@@ -8,6 +8,8 @@ module.exports = app => {
   //中间键
   const checkSignature = app.middleware.checkSignature(app.config);
   const interceptorXML2json = app.middleware.interceptorXML2json();
+  const identity = app.middleware.identity({});
+  const keyword = app.middleware.keyword();
   //test
   router.post('/', controller.home.index);
   router.get('/people/create', controller.test.create);
@@ -15,5 +17,5 @@ module.exports = app => {
 
 
   router.get('/wechat', controller.wechat.checkSignature);
-  router.post('/wechat', checkSignature, interceptorXML2json ,controller.wechat.post);
+  router.post('/wechat', interceptorXML2json, checkSignature, identity, keyword ,controller.wechat.post);
 };
