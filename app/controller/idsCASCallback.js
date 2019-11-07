1f9d30a3c7b0063d60cfc08a57f56a68108ee0d4
Wolf-Tungsten <canis.tungsten@foxmail.com>
2019-11-07 11:17:03 +0800

    测试统一身份认证

--- app/controller/idsCASCallback.js
+++ app/controller/idsCASCallback.js
@@ -1,53 +1,45 @@
-'use strict'
-const Controller = require('egg').Controller
-const sha1 = require('js-sha1')
-const moment = require('moment');
-const uuid = require('uuid/v4');
+'use strict';
+const Controller = require('egg').Controller;
 const axios = require('axios');
 const xmlparser = require('fast-xml-parser');
 
 class callbackController extends Controller {
   async callback() {
     // CAS认证
-    const { ctx } = this
+    const { ctx } = this;
     try {
-      let ids_session = ctx.params.idsSession
-      let ticket = ctx.request.query.ticket
-      //console.log(ticket)
-      //console.log(ids_session)
-      let serviceValidateURL = `${this.ctx.app.config.casURL}authserver/serviceValidate?service=${this.ctx.app.config.serverURL}idsCallback/${ids_session}&ticket=${ticket}`
+      const ids_session = ctx.params.idsSession;
+      const ticket = ctx.request.query.ticket;
+      // console.log(ticket)
+      // console.log(ids_session)
+      const serviceValidateURL = `${this.ctx.app.config.casURL}authserver/serviceValidate?service=${this.ctx.app.config.serverURL}idsCallback/${ids_session}&ticket=${ticket}`;
 
-      //测试用
-      //let serviceValidateURL = `${ctx.app.config.casURL}authserver/serviceValidate?service=https://seicwxbz.seu.edu.cn/idsCallback/${ids_session}&ticket=${ticket}`
+      // 测试用
+      // let serviceValidateURL = `${ctx.app.config.casURL}authserver/serviceValidate?service=https://seicwxbz.seu.edu.cn/idsCallback/${ids_session}&ticket=${ticket}`
 
-      let res = await axios.get(serviceValidateURL)
-      console.log(xmlparser.parse(res.data)['cas:serviceResponse'])
-      let data = xmlparser.parse(res.data)['cas:serviceResponse']['cas:authenticationSuccess']['cas:attributes']
-      let idsRecord = await ctx.model.Ids.findOne({ idsSession: ids_session })
-      let newPerson = new ctx.model.User({
+      const res = await axios.get(serviceValidateURL);
+      console.log(xmlparser.parse(res.data)['cas:serviceResponse']);
+      const data = xmlparser.parse(res.data)['cas:serviceResponse']['cas:authenticationSuccess']['cas:attributes'];
+      const idsRecord = await ctx.model.Ids.findOne({ idsSession: ids_session });
+      const newPerson = new ctx.model.User({
         openid: idsRecord.openId,
-        
-        //假的
-        name:'qnmd产品经理',
-
-        //name: data['cas:cn'],
+        name: data['cas:cn'],
         cardnum: data['cas:uid'],
-      })
-      await newPerson.save()
+      });
+      console.log(newPerson);
+      await newPerson.save();
 
       // 向用户推送redirectURL,
       // 只在wehcatOAuth发放token
-      console.log('继续跳转wechatOAuth')
-      let redirectURL = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${ctx.app.config.wechat.appID}&redirect_uri=${ctx.app.config.serverURL}wechatOauth&response_type=code&scope=snsapi_base&state=${idsRecord.target}#wechat_redirect`
-      ctx.redirect(redirectURL)
+      console.log('继续跳转wechatOAuth');
+      const redirectURL = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${ctx.app.config.wechat.appID}&redirect_uri=${ctx.app.config.serverURL}wechatOauth&response_type=code&scope=snsapi_base&state=${idsRecord.target}#wechat_redirect`;
+      ctx.redirect(redirectURL);
 
 
     } catch (e) {
-      console.log(e)
-      throw ('统一身份认证又崩啦')
+      console.log(e);
+      throw ('统一身份认证又崩啦');
     }
-
-
 
 
   }
--- app/service/wechatAction.js
+++ app/service/wechatAction.js
@@ -1,111 +1,110 @@
-'use strict'
-const Service = require('egg').Service
-const moment = require('moment')
-const uuid = require('uuid/v4')
+'use strict';
+const Service = require('egg').Service;
+const moment = require('moment');
+const uuid = require('uuid/v4');
 
 class keywordsService extends Service {
-    async process(ctx) {
-        let dispatchKeywords = {
-            '初始化管理员': this.initAdmin,
-            '管理后台': this.configUI,
-            '任务清单': this.todolList,
-        }
-        let dispatchClickEvent = {
-            '故障申报': "post",
-            '处理进度': "list_USER"
-        }
-        console.log(ctx.request.body)
-        if (dispatchKeywords[ctx.request.body.Content]) {
-            //响应关键字
-            let res = await dispatchKeywords[ctx.request.body.Content](ctx.request.body, ctx)
-            console.log(res)
-            ctx.status = 200
-            if (!res) {
-                ctx.body = 'success'
-            } else {
-                ctx.body = `<xml>
+  async process(ctx) {
+    const dispatchKeywords = {
+      初始化管理员: this.initAdmin,
+      管理后台: this.configUI,
+      任务清单: this.todolList,
+    };
+    const dispatchClickEvent = {
+      故障申报: 'post',
+      处理进度: 'list_USER',
+    };
+    console.log(ctx.request.body);
+    if (dispatchKeywords[ctx.request.body.Content]) {
+      // 响应关键字
+      const res = await dispatchKeywords[ctx.request.body.Content](ctx.request.body, ctx);
+      console.log(res);
+      ctx.status = 200;
+      if (!res) {
+        ctx.body = 'success';
+      } else {
+        ctx.body = `<xml>
                     <ToUserName><![CDATA[${ctx.request.body.FromUserName}]]></ToUserName>
                     <FromUserName><![CDATA[${ctx.request.body.ToUserName}]]></FromUserName>
                     <CreateTime>${+moment()}</CreateTime>
                     <MsgType><![CDATA[text]]></MsgType>
                     <Content><![CDATA[${res}]]></Content>
-                </xml>`
-            }
-        } else if (dispatchClickEvent[ctx.request.body.EventKey]) {
-            //响应click事件
-            let resOfOpenid = await ctx.model.User.findOne({ openid: ctx.request.body.FromUserName })
-            if (resOfOpenid) {
-                // 用户已经绑定
-                let state = dispatchClickEvent[ctx.request.body.EventKey]
-                let redirectURL = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${ctx.app.config.wechat.appID}&redirect_uri=${ctx.app.config.serverURL}wechatOauth&response_type=code&scope=snsapi_base&state=${state}#wechat_redirect`
-                let content = `<a href="${redirectURL}">点击继续</a>`
-                ctx.body = `<xml>
+                </xml>`;
+      }
+    } else if (dispatchClickEvent[ctx.request.body.EventKey]) {
+      // 响应click事件
+      const resOfOpenid = await ctx.model.User.findOne({ openid: ctx.request.body.FromUserName });
+      if (resOfOpenid) {
+        // 用户已经绑定
+        const state = dispatchClickEvent[ctx.request.body.EventKey];
+        const redirectURL = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${ctx.app.config.wechat.appID}&redirect_uri=${ctx.app.config.serverURL}wechatOauth&response_type=code&scope=snsapi_base&state=${state}#wechat_redirect`;
+        const content = `<a href="${redirectURL}">点击继续</a>`;
+        ctx.body = `<xml>
                     <ToUserName><![CDATA[${ctx.request.body.FromUserName}]]></ToUserName>
                     <FromUserName><![CDATA[${ctx.request.body.ToUserName}]]></FromUserName>
                     <CreateTime>${+moment()}</CreateTime>
                     <MsgType><![CDATA[text]]></MsgType>
                     <Content><![CDATA[${content}]]></Content>
-                </xml>`
-            } else {
-                //用户没有绑定
-                let idsSession = uuid()
-                let newIds = new ctx.model.Ids({
-                    idsSession: idsSession,
-                    openId: ctx.request.body.FromUserName,
-                    target: dispatchClickEvent[ctx.request.body.EventKey]
-                })
-                let res = await newIds.save()
-                let content = `<a href="https://myseu.cn" data-miniprogram-appid="wxaef6d2413690047f" data-miniprogram-path="pages/idsAuth?APPID=${ctx.app.config.wechat.appID}&IDS_SESSION=${idsSession}&FORCE=1">统一身份认证登录</a>`
-                ctx.body = `<xml>
+                </xml>`;
+      } else {
+        // 用户没有绑定
+        const idsSession = uuid();
+        const newIds = new ctx.model.Ids({
+          idsSession,
+          openId: ctx.request.body.FromUserName,
+          target: dispatchClickEvent[ctx.request.body.EventKey],
+        });
+        await newIds.save();
+        const content = `<a href="https://myseu.cn" data-miniprogram-appid="wxaef6d2413690047f" data-miniprogram-path="pages/idsAuth?APPID=${ctx.app.config.wechat.appID}&IDS_SESSION=${idsSession}&FORCE=1">统一身份认证登录</a>`;
+        ctx.body = `<xml>
                     <ToUserName><![CDATA[${ctx.request.body.FromUserName}]]></ToUserName>
                     <FromUserName><![CDATA[${ctx.request.body.ToUserName}]]></FromUserName>
                     <CreateTime>${+moment()}</CreateTime>
                     <MsgType><![CDATA[text]]></MsgType>
                     <Content><![CDATA[${content}]]></Content>
-                </xml>`
-            }
-        }
-        else {
-            ctx.body = 'success'
-        }
+                </xml>`;
+      }
+    } else {
+      ctx.body = 'success';
     }
+  }
 
-    async initAdmin(body, ctx) {
-        console.log(body)
-        let openid = body.FromUserName
-        // 判断是否已经初始化过
-        let adminCount = await ctx.model.User.countDocuments({ isAdmin: true })
-        if (adminCount > 0) {
-            return '非法操作，此事件将被报告'
-        }
-        // 首先判断数据库是否存在该用户
-        let user = await ctx.model.User.findOne({ openid })
-        if (user) {
-            user.isAdmin = true
-            await user.save()
-        } else {
-            user = new ctx.model.User({
-                openid,
-                isAdmin: true,
-                token: 'fake_token',
-                tokenExpireTime: 0
-            })
-            await user.save()
-        }
-        return '您已成为系统的初始管理员'
+  async initAdmin(body, ctx) {
+    console.log(body);
+    const openid = body.FromUserName;
+    // 判断是否已经初始化过
+    const adminCount = await ctx.model.User.countDocuments({ isAdmin: true });
+    if (adminCount > 0) {
+      return '非法操作，此事件将被报告';
     }
+    // 首先判断数据库是否存在该用户
+    let user = await ctx.model.User.findOne({ openid });
+    if (user) {
+      user.isAdmin = true;
+      await user.save();
+    } else {
+      user = new ctx.model.User({
+        openid,
+        isAdmin: true,
+        token: 'fake_token',
+        tokenExpireTime: 0,
+      });
+      await user.save();
+    }
+    return '您已成为系统的初始管理员';
+  }
 
-    async configUI(body, ctx) {
-        let openid = body.FromUserName
-        let user = await ctx.model.User.findOne({ openid })
-        if (user && user.isAdmin) {
-            return `<a href="${ctx.helper.oauthUrl(ctx, 'config')}">点击打开后台管理</a>`
-        }
+  async configUI(body, ctx) {
+    const openid = body.FromUserName;
+    const user = await ctx.model.User.findOne({ openid });
+    if (user && user.isAdmin) {
+      return `<a href="${ctx.helper.oauthUrl(ctx, 'config')}">点击打开后台管理</a>`;
     }
+  }
 
-    async todolList(body, ctx) {
-        return `<a href="${ctx.helper.oauthUrl(ctx, 'list', 'STAFF')}">点击查看任务清单</a>`
-    }
+  async todolList(body, ctx) {
+    return `<a href="${ctx.helper.oauthUrl(ctx, 'list', 'STAFF')}">点击查看任务清单</a>`;
+  }
 
 }
 
