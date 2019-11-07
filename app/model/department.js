9a04c42cb74fae1635873ba1fbb1ddaadd2bc4b6
ZZJ <zhaozjseu@163.com>
2019-11-07 21:47:22 +0800

    添加新的model

--- app/model/department.js
+++ app/model/department.js
@@ -1,11 +1,11 @@
-'use strict'
+'use strict';
 
-module.exports = app =>{
-    const mongoose = app.mongoose;
-    const Schema = mongoose.Schema;
-    const department = new Schema({
-        name:{type:String},  //部门名称
-        delete:{type:Boolean, default:false} // 是否标记删除
-    });
-    return mongoose.model('Department',department);
-}
+module.exports = app => {
+  const mongoose = app.mongoose;
+  const Schema = mongoose.Schema;
+  const department = new Schema({
+    name: { type: String }, // 部门名称
+    delete: { type: Boolean, default: false }, // 是否标记删除
+  });
+  return mongoose.model('Department', department);
+};
--- app/model/departmentAdminBind.js
+++ app/model/departmentAdminBind.js
@@ -3,11 +3,11 @@
 module.exports = app => {
   const mongoose = app.mongoose;
   const Schema = mongoose.Schema;
-  const StaffBind = new Schema({
+  const departmentAdminBind = new Schema({
     departmentId: { type: String }, // 部门ID
     departmentName: { type: String }, // 部门名称
-    staffCardnum: { type: String }, // 工作人员一卡通号
+    AdminCardnum: { type: String }, // 管理员一卡通号
     name: { type: String }, // 用户姓名
   });
-  return mongoose.model('StaffBind', StaffBind);
+  return mongoose.model('DepartmentAdminBind', departmentAdminBind);
 };
