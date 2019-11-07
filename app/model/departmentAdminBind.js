3177e0b15308660fc0051187fadd1a597f052416
ZZJ <zhaozjseu@163.com>
2019-11-07 21:48:20 +0800

    啊

--- app/model/departmentAdminBind.js
+++ app/model/departmentAdminBind.js
@@ -6,7 +6,7 @@ module.exports = app => {
   const departmentAdminBind = new Schema({
     departmentId: { type: String }, // 部门ID
     departmentName: { type: String }, // 部门名称
-    AdminCardnum: { type: String }, // 管理员一卡通号
+    adminCardnum: { type: String }, // 管理员一卡通号
     name: { type: String }, // 用户姓名
   });
   return mongoose.model('DepartmentAdminBind', departmentAdminBind);
