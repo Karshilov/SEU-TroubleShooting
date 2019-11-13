d3df443ef0ba5f5842fadf6072754edd9a9bd773
ZZJ <zhaozjseu@163.com>
2019-11-13 21:45:29 +0800

    测试

--- app/middleware/identity.js
+++ app/middleware/identity.js
@@ -5,6 +5,7 @@ module.exports = () => {
   return async function identity(ctx, next) {
 
     const token = ctx.request.headers.token;
+    console.log(token);
     const record = token ? await ctx.model.User.findOne({ token }) : false;
     if (record && record.tokenExpireTime > +moment()) {
       ctx.userInfo = record;
