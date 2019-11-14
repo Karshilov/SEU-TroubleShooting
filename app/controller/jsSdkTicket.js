b7568208a29d8221da0b0292bc5a4768e589ae5e
Wolf-Tungsten <canis.tungsten@foxmail.com>
2019-11-14 10:16:29 +0800

    支持外部应用获取jssdkTicket

--- app/controller/jsSdkTicket.js
+++ app/controller/jsSdkTicket.js
@@ -5,13 +5,16 @@ const Controller = require('egg').Controller;
 class JsSdkTicketController extends Controller {
   async index() {
     const { ctx } = this;
+    let extraUrl = ctx.query.extraUrl;
+    extraUrl = extraUrl ? decodeURI(extraUrl) : null;
     return {
       debug: false,
-      ...(await ctx.service.getAccessToken.jsSdkTicket()),
+      ...(await ctx.service.getAccessToken.jsSdkTicket(extraUrl)),
       appId: ctx.app.config.wechat.appID,
       jsApiList: [ 'chooseImage', 'previewImage', 'uploadImage', 'hideAllNonBaseMenuItem', 'hideOptionMenu' ],
     };
   }
+
 }
 
 module.exports = JsSdkTicketController;
