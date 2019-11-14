6b387a35ad0dacfd188789353740e741466ea180
Wolf-Tungsten <canis.tungsten@foxmail.com>
2019-11-14 21:05:15 +0800

    修改推送卡片消息格式

--- app/service/pushNotification.js
+++ app/service/pushNotification.js
@@ -1,113 +1,112 @@
-'use strict'
-const Service = require('egg').Service
-const moment = require('moment')
+'use strict';
+const Service = require('egg').Service;
 
 class pushNotification extends Service {
-    async userNotification(cardnum, title, address, type, status, lastModifiedTime, remark, url) {
-        let access_token = await this.service.getAccessToken.accessToken();
-        let user = await this.ctx.model.User.findOne({ cardnum });
-        if (!user) {
-            return
-        }
-        let postJson = {
-            "touser": user.openid,
-            "template_id": this.config.wechat.userTemplateId,
-            "url": url,
-            "data": {
-                "first": {
-                    "value": title+'\n',
-                    "color": "#173177"
-                },
-                "keyword1": {
-                    "value": address+'\n',
-                    "color": "#173177"
-                },
-                "keyword2": {
-                    "value": type+'\n',
-                    "color": "#173177"
-                },
-                "keyword3": {
-                    "value": status+'\n',
-                    "color": "#173177"
-                },
-                "keyword4": {
-                    "value": lastModifiedTime+'\n',
-                    "color": "#173177"
-                },
-                "remark": {
-                    "value": '\n'+remark+'\n',
-                    "color": "#173177"
-                }
-            }
-        }
-        let notificationURL = `https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=${access_token}`;
-        let result = await this.ctx.curl(notificationURL, {
-            method: 'POST',
-            dataType: 'json',
-            data: JSON.stringify(postJson)
-        })
-        
-        if(result.data.errcode){
-            console.log(`故障单处理进度通知发送失败 错误码：${result.data.errcode}`)
-        }
+  async userNotification(cardnum, title, address, type, status, lastModifiedTime, remark, url) {
+    const access_token = await this.service.getAccessToken.accessToken();
+    const user = await this.ctx.model.User.findOne({ cardnum });
+    if (!user) {
+      return;
+    }
+    const postJson = {
+      touser: user.openid,
+      template_id: this.config.wechat.userTemplateId,
+      url,
+      data: {
+        first: {
+          value: title,
+          color: '#173177',
+        },
+        keyword1: {
+          value: address,
+          color: '#173177',
+        },
+        keyword2: {
+          value: type,
+          color: '#173177',
+        },
+        keyword3: {
+          value: status,
+          color: '#173177',
+        },
+        keyword4: {
+          value: lastModifiedTime,
+          color: '#173177',
+        },
+        remark: {
+          value: remark,
+          color: '#173177',
+        },
+      },
+    };
+    const notificationURL = `https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=${access_token}`;
+    const result = await this.ctx.curl(notificationURL, {
+      method: 'POST',
+      dataType: 'json',
+      data: JSON.stringify(postJson),
+    });
 
+    if (result.data.errcode) {
+      console.log(`故障单处理进度通知发送失败 错误码：${result.data.errcode}`);
+    }
+
+
+  }
+
+  async staffNotification(cardnum, title, code, type, desc, phonenum, createdTime, remark, url) {
+    const access_token = await this.service.getAccessToken.accessToken();
+    const user = await this.ctx.model.User.findOne({ cardnum });
+    if (!user) {
+      return;
+    }
+    const postJson = {
+      touser: user.openid,
+      template_id: this.config.wechat.staffTemplateId,
+      url,
+      data: {
+        first: {
+          value: title,
+          color: '#173177',
+        },
+        keyword1: {
+          value: code,
+          color: '#173177',
+        },
+        keyword2: {
+          value: type,
+          color: '#173177',
+        },
+        keyword3: {
+          value: desc,
+          color: '#173177',
+        },
+        keyword4: {
+          value: phonenum,
+          color: '#173177',
+        },
+        keyword5: {
+          value: createdTime,
+          color: '#173177',
+        },
+        remark: {
+          value: remark,
+          color: '#173177',
+        },
+      },
+    };
+    const notificationURL = `https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=${access_token}`;
+    const result = await this.ctx.curl(notificationURL, {
+      method: 'POST',
+      dataType: 'json',
+      data: JSON.stringify(postJson),
+    });
+
+    if (result.data.errcode) {
+      console.log(`故障报修通知发送失败   错误码：${result.data.errcode}`);
 
     }
 
-    async staffNotification(cardnum, title, code, type, desc, phonenum, createdTime, remark, url) {
-        let access_token = await this.service.getAccessToken.accessToken();
-        let user = await this.ctx.model.User.findOne({ cardnum });
-        if (!user) {
-            return
-        }
-        let postJson = {
-            "touser": user.openid,
-            "template_id": this.config.wechat.staffTemplateId,
-            "url": url,
-            "data": {
-                "first": {
-                    "value": title+'\n',
-                    "color": "#173177"
-                },
-                "keyword1": {
-                    "value": code+'\n',
-                    "color": "#173177"
-                },
-                "keyword2": {
-                    "value": type+'\n',
-                    "color": "#173177"
-                },
-                "keyword3": {
-                    "value": desc+'\n',
-                    "color": "#173177"
-                },
-                "keyword4": {
-                    "value": phonenum+'\n',
-                    "color": "#173177"
-                },
-                "keyword5": {
-                    "value": createdTime+'\n',
-                    "color": "#173177"
-                },
-                "remark": {
-                    "value": '\n'+remark+'\n',
-                    "color": "#173177"
-                }
-            }
-        }
-        let notificationURL = `https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=${access_token}`;
-        let result = await this.ctx.curl(notificationURL, {
-            method: 'POST',
-            dataType: 'json',
-            data: JSON.stringify(postJson)
-        })
-        
-        if(result.data.errcode){
-            console.log(`故障报修通知发送失败   错误码：${result.data.errcode}`)
-
-        }
-
-    }
+  }
 }
 
 module.exports = pushNotification;
