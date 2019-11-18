9bb8c7892f11a75e73a0b35986c063637ffa99c7
ZZJ <zhaozjseu@163.com>
2019-11-19 02:37:03 +0800

    emmm

--- app/extend/helper.js
+++ app/extend/helper.js
@@ -9,4 +9,11 @@ module.exports = {
   oauthUrl(ctx, after, afterArgs) {
     return `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${ctx.app.config.wechat.appID}&redirect_uri=${ctx.app.config.serverURL}wechatOauth&response_type=code&scope=snsapi_base&state=${after}${afterArgs ? '_' + afterArgs : ''}#wechat_redirect`;
   },
+  timeDifference(before, now) {
+    const hour = 60 * 60 * 1000;
+    const minute = 60 * 1000;
+    const hourReturn = Math.floor((now - before) / hour);
+    const minuteReturn = Math.round((now - before - hourReturn * hour) / minute);
+    return (hourReturn === 0 ? '' : hourReturn + '小时') + minuteReturn + '分钟';
+  }
 };
