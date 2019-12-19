5ac83e3dd73b384777daaa4648825c5f58e3c8a1
ZZJ <zhaozjseu@163.com>
2019-12-19 23:08:16 +0800

    给金智token

--- app/controller/seicWisedu.js
+++ app/controller/seicWisedu.js
@@ -2,12 +2,28 @@
 
 const Controller = require('egg').Controller;
 const sha1 = require('sha1');
-// const moment = require('moment');
+const moment = require('moment');
+const uuid = require('uuid/v4');
 
 
 class wiseduController extends Controller {
-  async getToken() {
+  async token() {
     const { ctx } = this;
+    const { timestamp, signature } = ctx.query;
+    let now = +moment();
+    // 签名验证
+    if (signature !== sha1(this.config.seicApiKey + timestamp + this.config.seicSecret)) {
+      ctx.identityError('身份验证失败');
+    }
+    await ctx.model.WiseduTicket.deleteMany({});
+    now = +moment();
+    const newToken = new ctx.model.WiseduTicket({
+      token: uuid(),
+      expireTime: now + 7200 * 1000,
+    });
+    await newToken.save();
+    return newToken.token;
+
   }
 }
 
