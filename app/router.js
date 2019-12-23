d1b4e9f36e5e6d25d954268381d2fd624913d051
wolf-tungsten <canis.tungsten@foxmail.com>
2019-12-23 22:13:31 +0800

    增加转发路由

--- app/router.js
+++ app/router.js
@@ -67,8 +67,9 @@ module.exports = app => {
   router.get('/connect/token', controller.connect.token);
   router.post('/connect/submit', controller.connect.submit);
   router.post('/connect/accept', controller.connect.accept);
-  router.post('/connect/reject', controller.connect.reject);
   router.post('/connect/accomplish', controller.connect.accomplish);
+  router.post('/connect/redirect', controller.connect.redirect);
+  router.post('/connect/reject', controller.connect.reject);
   router.post('/connect/hasten', controller.connect.hasten);
   router.post('/connect/confirm', controller.connect.confirm);
 };
